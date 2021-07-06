79812a10181b35bdfb26970969a045a0d1b2e8ca
kubota <work.yukina.k.614@gmail.com>
2021-07-06 11:48:33 +0900

    正規表現バリデーションクラスのUnitTest #1

---
+++ src/main/java/com/example/samplejavaunittest/SampleJavaUnitTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.samplejavaunittest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleJavaUnitTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleJavaUnitTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/samplejavaunittest/SampleJavaUnitTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.samplejavaunittest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleJavaUnitTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
